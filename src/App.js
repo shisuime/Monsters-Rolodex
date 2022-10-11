f4e0860f5b2e4be83ada05b123b37c4e8862b510
Gunarnab Sinha <shisuime@gmail.com>
2022-10-11 07:35:04 -0700

    made components organised

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/Search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+  render() {
+    console.log("render from app.js");
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filterredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <SearchBox
+          className="search-box"
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+        />
+        <CardList monsters={filterredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
